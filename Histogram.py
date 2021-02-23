20d5861926a47409cf126d65548158f69302eac2
punya-ku <thebluefields25@gmail.com>
2021-02-23 20:15:18 +0800

    coment

--- Histogram.py
+++ Histogram.py
@@ -1,11 +1,19 @@
+#Pandas libary python digunakan untuk membaca file csv
 import pandas as pd
+#Matplotlib memvisualisasikan data secara 2D atau 3D
 import matplotlib.pyplot as plt
+#Visualisasi data secara statistik agar tampak lebih menarik
 import seaborn as sns
 
+#Membaca file csv dan meyimpannya pada variabel belajar
 belajar = pd.read_csv("Dataset/insurance.csv")
+#menampilkan semua data
 belajar.head()
 
+#memvisualisasikan distribusi data dengan interval 10
 sns.histplot(belajar['bmi'], bins=10)
 plt.xlabel('interval 10')
 plt.ylabel('frekuensi')
+
+#menampilkan hasil visualisasi
 plt.show()
--- Scatter Plot.py
+++ Scatter Plot.py
@@ -1,12 +1,19 @@
+#Pandas libary python digunakan untuk membaca file csv
 import pandas as pd
 import warnings ##untuk mengabaikan perintah warning pada seaborn
 warnings.filterwarnings("ignore")
+#Matplotlib memvisualisasikan data secara 2D atau 3D
 import matplotlib.pyplot as plt
+#Visualisasi data secara statistik agar tampak lebih menarik
 import seaborn as sns
 
+#Membaca file csv dan meyimpannya pada variabel belajar
 belajar = pd.read_csv("Dataset/insurance.csv")
+#menampilkan semua data
 belajar.head()
 
+#memvisualisasikan distribusi data dengan x (age) dan y (bmi)
 sns.FacetGrid(belajar, hue="sex", size=5) .map(plt.scatter, "age", "bmi") .add_legend()
 
+#menampilkan hasil visualisasi
 plt.show()
